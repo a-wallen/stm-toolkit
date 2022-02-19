b3b1a797b478ca5a706f6de05ca53229161979a4
Justin Thoreson <thoresonjd@gmail.com>
2022-02-19 15:29:55 -0800

    Documentation

--- sentiment_analyzer/sentiment_analyzer.py
+++ sentiment_analyzer/sentiment_analyzer.py
@@ -27,40 +27,12 @@ class SentimentAnalyzer():
     def __init__(self) -> None:
         pass
 
-    # def _train(self) -> None:
-    #     # Retrieve subjective and objective documents, tokenize list
-    #     n_instances = 100
-    #     subj_docs = [(sent, 'subj') for sent in subjectivity.sents(categories='subj')[:n_instances]]
-    #     obj_docs = [(sent, 'obj') for sent in subjectivity.sents(categories='obj')[:n_instances]]
-    #     print(len(subj_docs), len(obj_docs))
-    #     print(obj_docs[0])
-
-    #     # Split subjective and objective instances to training and testing docs
-    #     train_subj_docs = subj_docs[:80]
-    #     test_subj_docs = subj_docs[80:100]
-    #     train_obj_docs = obj_docs[:80]
-    #     test_obj_docs = obj_docs[80:100]
-    #     training_docs = train_subj_docs + train_obj_docs
-    #     testing_docs = test_subj_docs + test_obj_docs
-
-    #     # Handle negations with unigram word features
-    #     sentilyzer = Sentilyzer()
-    #     all_words_neg = sentilyzer.all_words([mark_negation(doc) for doc in training_docs]) 
-    #     unigram_feats = sentilyzer.unigram_word_feats(all_words_neg, min_freq=4)
-    #     print(len(unigram_feats))
-    #     sentilyzer.add_feat_extractor(extract_unigram_feats, unigrams=unigram_feats)
-
-    #     # Apply features to obtain a feature-value representation of datasets
-    #     training_set = sentilyzer.apply_features(training_docs)
-    #     test_set = sentilyzer.apply_features(testing_docs)
-
-    #     # Train classifyer on training set
-    #     trainer = NaiveBayesClassifier.train
-    #     classifier = sentilyzer.train(trainer, training_set)
-    #     for key,value in sorted(sentilyzer.evaluate(test_set).items()):
-    #         print('{0}: {1}'.format(key, value))
-
     def _filter_gen(self, article_text: str) -> str:
+        """Filters article text to contain relevant words
+        
+        :param article_text: Article text
+        """
+
         stop_words = set(stopwords.words('english'))
         article_text = re.sub(r'[^\w\s]', ' ', article_text)
         words = word_tokenize(article_text)
@@ -69,6 +41,10 @@ class SentimentAnalyzer():
                 yield word
 
     def analyze(self, article : News) -> Sentiment:
+        """Generates a sentiment analysis report  for an article
+        
+        :param article: Alpaca News article
+        """
 
         # Article data to analyze
         article_data = [  
@@ -104,8 +80,9 @@ class SentimentAnalyzer():
     def __del__(self) -> None:
         pass
 
-def test() -> None:
-    article = News(
+def main() -> None:
+
+    dummy = News(
         24803233,
         "Benzinga's Top 5 Articles For 2021 — Or 'Who Let The Dog Out?'",
         "Sue Strachan",
@@ -140,7 +117,7 @@ def test() -> None:
     )
 
     sa = SentimentAnalyzer()
-    sentiment = sa.analyze(article)
+    sentiment = sa.analyze(dummy)
     print(
         f'Pos: {sentiment.positive}, ' +
         f'Neut: {sentiment.neutral}, ' +
@@ -149,4 +126,37 @@ def test() -> None:
     )
 
 if __name__ == "__main__":
-    test()
+    main()
+
+    # def _train(self) -> None:
+    #     # Retrieve subjective and objective documents, tokenize list
+    #     n_instances = 100
+    #     subj_docs = [(sent, 'subj') for sent in subjectivity.sents(categories='subj')[:n_instances]]
+    #     obj_docs = [(sent, 'obj') for sent in subjectivity.sents(categories='obj')[:n_instances]]
+    #     print(len(subj_docs), len(obj_docs))
+    #     print(obj_docs[0])
+    #
+    #     # Split subjective and objective instances to training and testing docs
+    #     train_subj_docs = subj_docs[:80]
+    #     test_subj_docs = subj_docs[80:100]
+    #     train_obj_docs = obj_docs[:80]
+    #     test_obj_docs = obj_docs[80:100]
+    #     training_docs = train_subj_docs + train_obj_docs
+    #     testing_docs = test_subj_docs + test_obj_docs
+    #
+    #     # Handle negations with unigram word features
+    #     sentilyzer = Sentilyzer()
+    #     all_words_neg = sentilyzer.all_words([mark_negation(doc) for doc in training_docs]) 
+    #     unigram_feats = sentilyzer.unigram_word_feats(all_words_neg, min_freq=4)
+    #     print(len(unigram_feats))
+    #     sentilyzer.add_feat_extractor(extract_unigram_feats, unigrams=unigram_feats)
+    #
+    #     # Apply features to obtain a feature-value representation of datasets
+    #     training_set = sentilyzer.apply_features(training_docs)
+    #     test_set = sentilyzer.apply_features(testing_docs)
+    #
+    #     # Train classifyer on training set
+    #     trainer = NaiveBayesClassifier.train
+    #     classifier = sentilyzer.train(trainer, training_set)
+    #     for key,value in sorted(sentilyzer.evaluate(test_set).items()):
+    #         print('{0}: {1}'.format(key, value))
