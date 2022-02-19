948733b4b22180e420849ae97e53936a69f02cc2
Stephen (Alex) Wallen <wallenstephen@outlook.com>
2022-02-19 01:38:49 -0800

    Add financial news container (timer trigger every 5 minutes)

---
+++ financial_news_scraper/__init__.py
@@ -0,0 +1,18 @@
+import sys
+# Append the path to the /models folder for access to shared models
+sys.path.append(os.path.join(os.path.dirname(__file__), 'models'))
+
+import datetime
+import logging
+
+import azure.functions as func
+
+
+def main(mytimer: func.TimerRequest) -> None:
+    utc_timestamp = datetime.datetime.utcnow().replace(
+        tzinfo=datetime.timezone.utc).isoformat()
+
+    if mytimer.past_due:
+        logging.info('The timer is past due!')
+
+    logging.info('Python timer trigger function ran at %s', utc_timestamp)
