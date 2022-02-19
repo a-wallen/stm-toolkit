a586191cbb1add337a68037c7835207f870ed469
Stephen (Alex) Wallen <wallenstephen@outlook.com>
2022-02-19 09:33:20 -0800

    Create sentiment class for sentiment analysis reports

--- models/sentiment.py
+++ models/sentiment.py
@@ -1,7 +1,17 @@
 class Sentiment():
-    pass
+    def __init__(
+        self,
+        article_url: str,
+        positive: float,
+        neutral: float,
+        negative: float,
+    ):
+        self.article_url = article_url
+        self.positive = positive
+        self.neutral = neutral
+        self.negative = negative
+        pass
 
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
-    a = Alpaca()
