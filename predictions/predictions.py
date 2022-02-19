ccd9f25324c10d0a0d69d80512d2ab907ff7523e
Stephen (Alex) Wallen <wallenstephen@outlook.com>
2022-02-19 09:33:37 -0800

    Create new predictions class

---
+++ predictions/predictions.py
@@ -0,0 +1,9 @@
+import sys
+# Append the path to the /models folder for access to shared models
+sys.path.append(os.path.join(os.path.dirname(__file__), 'models'))
+
+from prediction import prediction # prediction reports
+from cosmos import Cosmos # database client wrapper
+
+class Predictions():
+    pass
