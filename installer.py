93c67f3d98b1022b4a3f3b54530adb0a5e5a80e3
Stephen (Alex) Wallen <wallenstephen@outlook.com>
2022-02-19 01:38:23 -0800

    Create installer for team to quickly install packages locally

---
+++ installer.py
@@ -0,0 +1,19 @@
+from pip._internal import main as pipmain
+import platform
+
+def import_or_install(package):
+    try:
+        __import__(package)
+    except ImportError:
+        pipmain(['install', package])   
+
+def _getDependencies():
+    assert (int(platform.python_version_tuple()[0]) >= 3 and int(platform.python_version_tuple()[1]) >= 6), """
+        Python version is not greater than 3.6, some packages in this repository require python 3.6 >.
+    """
+    with open("requirements.txt") as requirements:
+        for package in [line.rstrip() for line in requirements if line.rstrip() and not line.startswith('#')]:
+            import_or_install(package)
+
+if __name__ == "__main__":
+    _getDependencies()
