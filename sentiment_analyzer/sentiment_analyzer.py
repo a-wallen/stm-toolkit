aefa704540a3e16bee563f91852259a10fb96387
Justin Thoreson <thoresonjd@gmail.com>
2022-02-19 15:58:42 -0800

    Delete remporary article reader

--- sentiment_analyzer/sentiment_analyzer.py
+++ sentiment_analyzer/sentiment_analyzer.py
@@ -1,4 +1,6 @@
-import sys, os, re
+import re
+import sys, os
+
 # Append the path to the /models folder for access to shared models
 sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'models'))
 
@@ -14,7 +16,6 @@ from nltk.sentiment import SentimentAnalyzer as Sentilyzer
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from nltk.sentiment.util import *
 
-
 # Stock Ticker info from Alpaca: https://alpaca.markets/docs/api-references/market-data-api/stock-pricing-data/historical/
 from alpaca import Alpaca # internal wrapper for getting stock info getting articles
 from alpaca_news import AlpacaNews as News
@@ -23,6 +24,7 @@ from cosmos import Cosmos # internal wrapper class for persisting data
 from sentiment import Sentiment # internal class for storing sentiment data
 
 class SentimentAnalyzer():
+    """Sentiment Analyzer for Alpaca News articles"""
 
     def __init__(self) -> None:
         pass
