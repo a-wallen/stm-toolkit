7fcd4cfba8866672a76ba0bdc133395b4daa98bd
Justin Thoreson <thoresonjd@gmail.com>
2022-02-19 19:17:00 -0800

    Predictions class

--- predictions/predictions.py
+++ predictions/predictions.py
@@ -1,10 +1,15 @@
 import sys, os
 
 # Append the path to the /models folder for access to shared models
-sys.path.append(os.path.join(os.path.dirname(__file__), 'models'))
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'models'))
 
-from prediction import prediction # prediction reports
+from prediction import Prediction # prediction reports
 from cosmos import Cosmos # database client wrapper
 
 class Predictions():
-    pass
+
+    def __init__(self):
+        pass
+
+    def __del__(self):
+        pass
