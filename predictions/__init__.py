557ea3699ebf39754e5c427b17d9a1a6461ba8f1
Justin Thoreson <thoresonjd@gmail.com>
2022-02-19 19:22:43 -0800

    Predictions started

--- predictions/__init__.py
+++ predictions/__init__.py
@@ -1,11 +1,21 @@
-import sys
+import sys, os
 # Append the path to the /models folder for access to shared models
 sys.path.append(os.path.join(os.path.dirname(__file__), 'models'))
 
 import logging
-
 import azure.functions as func
+from cosmos import Cosmos
+from sentiment import Sentiment
 
 def main(documents: func.DocumentList) -> str:
+
     if documents:
         logging.info('Document id: %s', documents[0]['id'])
+
+    # Access database
+    instance = Cosmos()
+
+    # Read sentiment data
+    sentiments = instance.read(Sentiment) # filter_injection
+
+    # TODO: Create predictions
